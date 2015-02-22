066ace079665f6777dfd4e0b4a7161d14caefd0e
Gabriel Acosta <joga.acosta@gmail.com>
2015-02-21 21:36:42 -0600

    .

--- materias/App.java
+++ materias/App.java
@@ -10,8 +10,10 @@ import java.io.FileReader;
 import java.util.Scanner;
 import java.io.EOFException;
 import java.io.OptionalDataException;
+import java.lang.ClassCastException;
 public class App {
     public static void main(String arg[]) throws IOException   {
+	
 	Materia materias[] = {
 	    new Materia("Calculo", 9),
 	    new Materia("Ingles", 9.5),
@@ -24,10 +26,12 @@ public class App {
 	File f = new File("listado.txt");
 
 	ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(f));
+	out.writeObject(new Parasito());
 	for(Materia m : materias) {
 	    out.writeObject(m);
 	}
 	out.writeInt(3223);
+	out.writeObject(new Parasito());
 	out.close();
 
 	ObjectInputStream in = new ObjectInputStream(new FileInputStream(f));
@@ -40,10 +44,29 @@ public class App {
 		} catch(ClassNotFoundException nf) {
 		    System.out.println("No se encontro la clase, se ignorara");
 		} catch(OptionalDataException ode) {
-		    System.out.println("Se encontro un dato primitivo");
+		    // Excepcion lanzada cuando se encuentra un dato primitivo
+		    // al leer el archivo.
+		    System.out.println("Error: se intento leer un dato primitivo");
+		    //ode.printStackTrace();
+		    
+		    /*
+		      La clase OptionalDataException tiene un atributo publico length, 
+		      donde se almacena el numero de bytes encontrados de datos primitivos.
+		      Usamos el metodo skip() de ObjectInputStream, pasando como argumento
+		      el numero de bytes, para saltar el numero de bytes encontrados.
+		    */
+		    in.skip(ode.length); 
+		} catch (ClassCastException cce){
+		    System.out.println("Error: Encontrado un objeto diferente a Materia");
 		}
 	    }
 	} catch(EOFException eof) {
+	    /*
+	      La clase scanner no cuenta con una funcion que determine EOF,
+	      sin embargo, lanza una excepcion cuando se ha encontrado el final
+	      del archivo. Se ha optado por hacerlo de esta manera para leer 
+	      una cantidad arbitraria de objetos.
+	     */
 	    in.close();
 	}
     }
